abdc41bd51408f6bdf916cbb0a6b415ae62e8644
ChrisW <chris_white_12@hotmail.com>
2025-03-30 19:27:37 +1000

    have select squre change, working on tileBuilder

--- src/components/TileSelector.tsx
+++ src/components/TileSelector.tsx
@@ -13,8 +13,11 @@ const TileSelector = ({ }: TileSelectorProps) => {
 
     const draw = () => {
         const canvas = document.getElementById('tileSelector') as HTMLCanvasElement;
+        const selectCanvas = document.getElementById('selectSquare') as HTMLCanvasElement;
         canvas.width = tileAtlas.width;
         canvas.height = tileAtlas.height;
+        selectCanvas.width = tileAtlas.width;
+        selectCanvas.height = tileAtlas.height;
         const context = canvas.getContext('2d');
         if (context) {
             context.drawImage(tileAtlas, 0, 0);
@@ -76,6 +79,22 @@ const TileSelector = ({ }: TileSelectorProps) => {
         setTileBuildSize(parseInt(event.target.value));
     }
 
+    const handleTileBuildClick = (event: MouseEvent<HTMLCanvasElement>) => {
+        const canvas1 = document.getElementById('tileBuilder') as HTMLCanvasElement;
+        const rect = canvas1.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        const xPos = Math.floor(x / tileSize);
+        const yPos = Math.floor(y / tileSize);
+        const ctx1 = canvas1.getContext('2d');
+        if (!ctx1) {
+            return;
+        }
+        ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+        // TODO - having trouble placing the tile in the right position
+        putTile([[0,0]], xPos, yPos, ctx1, 1, tileSize, tileAtlas);
+    }
+
     useEffect(() => {
         tileAtlas.src = '/tileset_arranged.png';
         tileAtlas.onload = () => {
@@ -88,12 +107,15 @@ const TileSelector = ({ }: TileSelectorProps) => {
     }, []);
 
     useEffect(() => {
-        const canvas = document.getElementById('tileSelector') as HTMLCanvasElement;
+        const canvas = document.getElementById('selectSquare') as HTMLCanvasElement;
         const ctx = canvas.getContext('2d');
-        if (!ctx) {
+        const previewCanvas = document.getElementById('tilePreview') as HTMLCanvasElement;
+        const previewCtx = previewCanvas.getContext('2d');
+        if (!ctx || !previewCtx) {
             return;
         }
-        ctx.clearRect(0, 0, tileSize, tileSize);
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.strokeStyle = 'yellow';
         ctx.lineWidth = 2;
         ctx.strokeRect(selectedTile.x * tileSize, selectedTile.y * tileSize, tileSize, tileSize);
@@ -105,28 +127,30 @@ const TileSelector = ({ }: TileSelectorProps) => {
     }, [tileBuildSize]);
 
     return (
-        <Stack direction='column' spacing={2}>
+        <Stack direction='column' spacing={2} sx={{ flex: 1, minHeight: '600px' }}>
             <Stack 
                 direction={'row'} 
-                sx={{ border: '1px solid black', padding: 2 }}
+                sx={{ padding: 2, flex: 1}}
                 justifyContent={'space-between'}    
             >
-                <Stack direction='column' spacing={2} alignItems={'center'}>
-                    <canvas id="tileSelector" onClick={handleTileClick}></canvas>
+                <Stack sx={{ position: 'relative', padding: 2, flex: 1 }}>
+                    <canvas id="tileSelector" style={{ position: "absolute" }}></canvas>
+                    <canvas id="selectSquare" onClick={handleTileClick} style={{ position: "absolute", zIndex: 1 }}></canvas>
+                    <Box>
+                        <canvas id="tilePreview" width={tileSize} height={tileSize}></canvas>
+                    </Box>
                 </Stack>
-                <Stack>
+                <Stack spacing={2} sx={{ flex: 1 }}>
                     <TextField 
                         label="Tile Size" 
                         value={tileBuildSize}
                         onChange={handleTileSizeChange}
                         type="number"
                     />
+                    <Box>
+                        <canvas id="tileBuilder" onClick={handleTileBuildClick} width={tileSize * tileBuildSize} height={tileSize * tileBuildSize}></canvas>
+                    </Box>
                 </Stack>
-            </Stack>
-            <Stack>
-                <Box>
-                    <canvas id="tileBuilder" width={tileSize * tileBuildSize} height={tileSize * tileBuildSize}></canvas>
-                </Box>
             </Stack>
         </Stack>
     );
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,13 +2,16 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import HeaderNavigation from '../components/HeaderNavigation'
 import '../App.css'
+import { Stack } from '@mui/material'
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Stack sx={{ width: '100%', height: '100vh' }} spacing={2}>
         <HeaderNavigation />
         <Outlet />
         <TanStackRouterDevtools />
+      </Stack>
     </>
   ),
 })
